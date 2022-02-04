7283b61dd32ee032e4dc9f7dd7658203d79befbf
Piet <pietvdw67@gmail.com>
2022-02-04 22:11:15 +0200

    Updated machinedetail retrieval sorting

--- src/main/java/com/infinity/ProductiveIO/machineDetail/repository/MachineDetailRepository.java
+++ src/main/java/com/infinity/ProductiveIO/machineDetail/repository/MachineDetailRepository.java
@@ -1,9 +1,15 @@
 package com.infinity.ProductiveIO.machineDetail.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.infinity.ProductiveIO.machineDetail.model.MachineDetail;
 
 public interface MachineDetailRepository extends JpaRepository<MachineDetail,Long> {
+	
+	@Query(value = "SELECT * from machinedetail order by id asc",nativeQuery = true)
+	List<MachineDetail> findBySortedById();
 
 }
--- src/main/java/com/infinity/ProductiveIO/machineDetail/resource/MachineDetailResource.java
+++ src/main/java/com/infinity/ProductiveIO/machineDetail/resource/MachineDetailResource.java
@@ -3,6 +3,7 @@ package com.infinity.ProductiveIO.machineDetail.resource;
 import java.util.List;
 import java.util.logging.Logger;
 
+import org.hibernate.annotations.Sort;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,8 +26,7 @@ public class MachineDetailResource {
 	@CrossOrigin(origins = "http://localhost:4200")
 	@GetMapping("/machinedetails/v1")
 	public List<MachineDetail> getMachineDetails() {
-		
-		return repository.findAll();
+		return repository.findBySortedById();
 	}
 	
 	@CrossOrigin(origins = "http://localhost:4200")
